1b5d63a70f35be531834ab8865202661355180cc
Edwin Llanos <ellanos@merqueo.com>
2019-01-14 22:37:09 -0500

    [Progress] First fields. -Create ProductsFragment with your viewModel. -Create xmls for animations. -Add navigation support.

--- app/src/main/java/com/tpaga/productstopay/MainActivity.kt
+++ app/src/main/java/com/tpaga/productstopay/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.tpaga.productstopay
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout)
+    }
+
+
+    private fun setupNavigation() {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+
+        setupActionBarWithNavController(this, navController, drawerLayout)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        setupWithNavController(navigationView, navController)
     }
 }
---
+++ app/src/main/java/com/tpaga/productstopay/presentation/ProductsViewModel.kt
@@ -0,0 +1,7 @@
+package com.tpaga.productstopay.presentation
+
+import androidx.lifecycle.ViewModel;
+
+class ProductsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
